15c6e75a49699ea5f78cb50680c67cb0005256eb
Kotipatruni Tirumala Rao <159798530+Tiru2002@users.noreply.github.com>
2025-01-07 09:56:44 +0530

    Time: 0 ms (100%), Space: 17.6 MB (25.16%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.py
+++ 0231-power-of-two/0231-power-of-two.py
@@ -3,6 +3,6 @@ class Solution:
     def isPowerOfTwo(self, n: int) -> bool:
         if n<=0:
             return False
-        # return math.sqrt(n)==int(math.sqrt(n))
-        return math.floor(math.log2(n))==math.ceil(math.log2(n))
-        #return n&(n-1)==0
+        #return math.sqrt(n)==int(math.sqrt(n))
+        #return math.floor(math.log2(n))==math.ceil(math.log2(n))
+        return n&(n-1)==0
